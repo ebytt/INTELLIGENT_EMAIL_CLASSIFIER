ed0c0d1e1848ec68eca3a0d696140e49645ba45f
Ebunlicious <126627073+ebytt@users.noreply.github.com>
2025-09-05 21:20:30 +0100

    Update spam_app.py

--- spam_app.py
+++ spam_app.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import pickle  as pkl
+import joblib
 import streamlit as st
 
 
@@ -11,11 +11,14 @@ It uses a Logistic Regression model trained on a dataset of emails.""")
 # -----> load the model and vectorizer
 # If You Used a CountVectorizer or TfidfVectorizer Before Training:
 # You must transform the new input using the same vectorizer before passing it to the model.
-with open("Logistic_model.pkl", "rb") as file:
-    loaded_model = pkl.load(file)
+# with open("Logistic_model.pkl", "rb") as file:
+#     loaded_model = pkl.load(file)
 
-with open("cv_model.pkl", "rb") as file:
-    vectorizer = pkl.load(file)
+# with open("cv_model.pkl", "rb") as file:
+#     vectorizer = pkl.load(file)
+
+loaded_model = joblib.load("Logistic_model.pkl")
+vectorizer = joblib.load("cv_model.pkl")
 
 # Sample input (ensure it's string)
 user_input = str(st.text_area("__Enter email text here__"))
@@ -41,6 +44,7 @@ st.divider()
 # Display the current year in the footer
 
 st.markdown("_Made with ❤️ by Ebunlicious_")
+
 
 
 
